4cd719858671f069ab461c5880c120b8b9660d65
max-machin <machin.max@lapalteforme.io>
2022-04-13 23:48:55 +0200

    Push de la page d'index et de recherche principale

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Red+Hat+Display:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&display=swap" rel="stylesheet">
+    <title>Autocompletion</title>
+</head>
+<body>
+    <nav>
+        <label class="logo">
+            G'wame
+        </label>
+        <ul class="menu">
+            <li><a href="">2022</a></li>
+            <li><a href="">2021</a></li>
+            <li><a href="">Tous les Jeux</a></li>
+        </ul>
+    </nav>
+    <main>
+        <section class="headAccueil">
+            <div>
+               <img src="images/GF'R.png" alt="">
+                <h1>'wame</h1> 
+            </div>
+            <p>Votre moteur de recherche spécialiste en jeux vidéos.</p>
+        </section>
+        <section class="card-holder">
+            <div class="card bg-spring">
+                <div class="search-wrap">
+                    <form class="search-box">
+                        <button name="submit" class="btn" type="submit">
+                            <i class="fa fa-search"></i>
+                        </button>
+                        <input class="input" type="text" placeholder="Search.." name="motClef">
+                    </form>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <div class="footerFlex">
+            <ul>
+                <li><a href="">About</a></li>
+                <li><a href="">FAQ</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+            <div>
+                <h2>Faciliter votre recherche</h2>
+                <p>Trouver un jeux vidéo n'a jamais été aussi simple.</p>
+            </div>
+        </div>
+        <ul class="social">
+            <li><a href=""><i class="fa fa-facebook"></i></a></li>
+            <li><a href=""><i class="fa fa-twitter"></i></a></li>
+            <li><a href=""><i class="fa fa-git"></i></a></li>
+            <li><a href=""><i class="fa fa-linkedin-square"></i></a></li>
+            <li><a href=""><i class="fa fa-google-plus"></i></a></li>
+        </ul>
+
+        <div class="footerEnd">
+            <p>Copyright ©2022 G'wame Corporation. Designed By <span>MACHIN MAX</span></p>
+        </div>
+    </footer>
+</body>
+</html>
